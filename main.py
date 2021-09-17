bc0328c545c6ca6efa1fbdd6a4a9f271ecbceda5
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-09-17 02:08:23 -0500

    Update main.blocks, main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+def CtoF(num: number, num2: number):
+    global F_Temp
+    F_Temp = temp * 2
+    F_Temp += 30
+    return F_Temp
+
+def on_received_number(receivedNumber):
+    basic.show_number(receivedNumber)
+radio.on_received_number(on_received_number)
+
 temp = 0
+F_Temp = 0
+radio.set_group(1)
+radio.set_transmit_power(7)
 
 def on_forever():
     global temp
     temp = input.temperature()
-    basic.show_number(temp)
+    CtoF(1, 1)
+    radio.send_number(F_Temp)
+    basic.show_number(F_Temp)
 basic.forever(on_forever)
