9260d247e616cbfb7ac7b29e081339f6191df536
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-09-17 03:53:56 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,18 +6,18 @@ function CtoF (num: number, num2: number) {
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
 })
-input.onButtonPressed(Button.A, function () {
-	
+input.onButtonPressed(Button.B, function () {
+    control.reset()
 })
 let temp = 0
 let F_Temp = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
-loops.everyInterval(1000, function () {
-    radio.sendNumber(F_Temp)
-    basic.showNumber(F_Temp)
-})
 basic.forever(function () {
     temp = input.temperature()
     CtoF(1, 1)
+})
+loops.everyInterval(100, function () {
+    radio.sendNumber(F_Temp)
+    basic.showNumber(F_Temp)
 })
